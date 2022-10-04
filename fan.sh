3d1eadb38893dea196a7ddf1ce1f63135b0cd6a8
vaughaag <98517568+vaughaag@users.noreply.github.com>
2022-10-04 12:26:34 +0000

    Initial

---
+++ fan.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+echo 900 > /sys/devices/platform/applesmc.768/fan1_min
+echo 1400 > /sys/devices/platform/applesmc.768/fan2_min
+echo 1400 > /sys/devices/platform/applesmc.768/fan3_min
+echo 900 > /sys/devices/platform/applesmc.768/fan4_min
+echo 3000 > /sys/devices/platform/applesmc.768/fan5_min
+echo 3000 > /sys/devices/platform/applesmc.768/fan6_min
